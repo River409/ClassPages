a8ef921cb0da63a576a9f4edaec2e36ec50b2382
River <blubendy8@gmail.com>
2026-08-05 15:55:05 +0200

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,17 @@
-console.log("Editor loaded");
+const params = new URLSearchParams(window.location.search);
+
+const domain = params.get("domain");
+
+const title = document.getElementById("domainTitle");
+
+if (!domain) {
+
+    title.innerHTML = "No domain selected.";
+
+} else {
+
+    title.innerHTML = "Editing " + domain;
+
+}
+
+console.log(domain);
