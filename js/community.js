7635ed65b57890b9f4ba38e70cf8658789e46d45
River <blubendy8@gmail.com>
2026-08-05 21:39:03 +0200

    Refactor community.js to improve website rendering

--- js/community.js
+++ js/community.js
@@ -5,35 +5,111 @@ import {
     getDocs
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const container =
+document.getElementById("communitySites");
 
-const container = document.getElementById("communitySites");
+const sitesPerPage = 8;
 
+let currentPage = 1;
+
+const websites = [];
 
 const snapshot = await getDocs(
     collection(db, "domains")
 );
 
-
-let html = "";
-
-
 snapshot.forEach(docSnap => {
 
-    const data = docSnap.data();
+    websites.push(docSnap.data());
 
+});
+
+function renderPage(){
+
+    if(websites.length === 0){
+
+        container.innerHTML = "No websites yet.";
+
+        return;
+
+    }
+
+    const totalPages =
+    Math.ceil(websites.length / sitesPerPage);
+
+    const start =
+    (currentPage - 1) * sitesPerPage;
+
+    const end =
+    start + sitesPerPage;
+
+    let html = `
+
+    <div class="communityGrid">
+
+    `;
+
+    websites
+    .slice(start,end)
+    .forEach(site=>{
+
+        html += `
+
+        <div class="communityCard">
+
+            <div class="communityIcon">
+
+                🌐
+
+            </div>
+
+            <h3>
+
+                ${site.domain}
+
+            </h3>
+
+            <button
+                class="button"
+                onclick="window.location.href='site.html?domain=${site.domain}'">
+
+                Visit Website
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+    html += "</div>";
 
     html += `
 
-    <div style="margin-bottom:15px">
+    <div class="pagination">
 
-        🌐 <b>${data.domain}</b>
+        <button
+            class="button"
+            id="prevPage"
+            ${currentPage===1?"disabled":""}>
 
-        <br><br>
+            ◀ Previous
 
-        <button class="button"
-        onclick="window.location.href='site.html?domain=${data.domain}'">
+        </button>
 
-        Visit Website
+        <span>
+
+            Page ${currentPage} of ${totalPages}
+
+        </span>
+
+        <button
+            class="button"
+            id="nextPage"
+            ${currentPage===totalPages?"disabled":""}>
+
+            Next ▶
 
         </button>
 
@@ -41,14 +117,36 @@ snapshot.forEach(docSnap => {
 
     `;
 
-});
+    container.innerHTML = html;
 
+    document
+    .getElementById("prevPage")
+    .onclick=()=>{
 
-if(html === ""){
+        if(currentPage>1){
 
-    html = "No websites yet.";
+            currentPage--;
+
+            renderPage();
+
+        }
+
+    };
+
+    document
+    .getElementById("nextPage")
+    .onclick=()=>{
+
+        if(currentPage<totalPages){
+
+            currentPage++;
+
+            renderPage();
+
+        }
+
+    };
 
 }
 
-
-container.innerHTML = html;
+renderPage();
