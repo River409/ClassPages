cd27890f86a41012b22383e36e45f98fe5ba75d0
River <blubendy8@gmail.com>
2026-08-05 16:41:52 +0200

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,6 +1,73 @@
+import { db } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
 const params = new URLSearchParams(window.location.search);
 
 const domain = params.get("domain");
 
-document.getElementById("website").innerHTML =
-    "<h1>" + domain + "</h1><p>Site loading...</p>";
+const website = document.getElementById("website");
+const userStyle = document.getElementById("userStyle");
+
+
+if (!domain) {
+
+    website.innerHTML = "❌ No domain selected.";
+
+}
+else {
+
+    loadWebsite();
+
+}
+
+
+async function loadWebsite() {
+
+    try {
+
+        const siteRef = doc(db, "sites", domain);
+
+        const snapshot = await getDoc(siteRef);
+
+
+        if (!snapshot.exists()) {
+
+            website.innerHTML =
+                `
+                ❌ Website not found.<br><br>
+                ${domain} has not been created yet.
+                `;
+
+            return;
+
+        }
+
+
+        const data = snapshot.data();
+
+
+        userStyle.innerHTML =
+            data.css || "";
+
+
+        website.innerHTML =
+            data.html || "<p>This website is empty.</p>";
+
+
+    }
+
+    catch(error) {
+
+        console.error(error);
+
+        website.innerHTML =
+            "❌ Error loading website.";
+
+    }
+
+}
