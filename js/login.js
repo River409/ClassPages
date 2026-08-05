ed991813ac9e0652d74e024c032237ce3570befb
River <blubendy8@gmail.com>
2026-08-05 16:25:37 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,16 @@
 console.log("LOGIN.JS LOADED");
 
-
 import {
     register,
-    login
+    login,
+    logout
 } from "./auth.js";
 
+import { auth } from "./firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const emailInput = document.getElementById("emailInput");
 const passwordInput = document.getElementById("passwordInput");
@@ -13,61 +18,63 @@ const passwordInput = document.getElementById("passwordInput");
 const loginButton = document.getElementById("loginButton");
 const signupButton = document.getElementById("signupButton");
 
-
-console.log({
-    emailInput,
-    passwordInput,
-    loginButton,
-    signupButton
-});
-
+const loginArea = document.getElementById("loginArea");
+const memberArea = document.getElementById("memberArea");
+const welcomeUser = document.getElementById("welcomeUser");
+const logoutButton = document.getElementById("logoutButton");
 
 signupButton.addEventListener("click", async () => {
 
-    console.log("SIGN UP CLICKED");
-
-
-    const email = emailInput.value;
+    const email = emailInput.value.trim();
     const password = passwordInput.value;
 
-
-    const user = await register(
-        email,
-        password
-    );
-
+    const user = await register(email, password);
 
     if(user){
 
-        alert(
-            "Account created successfully!"
-        );
+        alert("Account created!");
 
     }
 
 });
 
-
 loginButton.addEventListener("click", async () => {
 
-    console.log("LOGIN CLICKED");
-
-
-    const email = emailInput.value;
+    const email = emailInput.value.trim();
     const password = passwordInput.value;
 
-
-    const user = await login(
-        email,
-        password
-    );
-
+    const user = await login(email, password);
 
     if(user){
 
-        alert(
-            "Logged in successfully!"
-        );
+        alert("Logged in!");
+
+    }
+
+});
+
+logoutButton.addEventListener("click", async () => {
+
+    await logout();
+
+});
+
+onAuthStateChanged(auth, (user) => {
+
+    if(user){
+
+        loginArea.style.display = "none";
+
+        memberArea.style.display = "block";
+
+        welcomeUser.textContent =
+            "Welcome, " + user.email;
+
+    }else{
+
+        loginArea.style.display = "block";
+
+        memberArea.style.display = "none";
 
     }
 
