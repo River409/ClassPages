48e6fa61dcf771e46aadd6379835156105a43890
River <blubendy8@gmail.com>
2026-08-05 11:15:59 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,8 +3,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-    // Paste your config here
+  apiKey: "AIzaSyA4CEaopC5GiylLq3XjikaDFl0RjoHUQmM",
+  authDomain: "classpages-da6ea.firebaseapp.com",
+  projectId: "classpages-da6ea",
+  storageBucket: "classpages-da6ea.firebasestorage.app",
+  messagingSenderId: "1063369301604",
+  appId: "1:1063369301604:web:f99bf87a14831afd630971"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
